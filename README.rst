06377e8b2ec2ace63b901eeb4ff1983e2ef836ca
Jiangge Zhang <tonyseek@gmail.com>
2015-03-02 12:20:21 +0800

    initial commit.

---
+++ README.rst
@@ -0,0 +1,6 @@
+RSOCKS
+======
+
+RSOCKS is a reserve proxy server for transferring traffic of specific host
+through a SOCKS 4/5 proxy. It is useful for using SOCKS 4/5 in applications
+that don't have SOCKS proxy support.
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme:
+    long_description = ''.join(readme).strip()
+
+
+setup(
+    name='rsocks',
+    version='0.1.0.dev',
+    author='Jiangge Zhang',
+    author_email='tonyseek@gmail.com',
+    description='A SOCKS 4/5 reserve proxy server.',
+    long_description=long_description,
+    url='https://github.com/tonyseek/rsocks',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages(),
+    platforms=['Any'],
+)
