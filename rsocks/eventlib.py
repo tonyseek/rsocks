d4397f23eae4cc08c345eb1b0229bf232e8e771f
Jiangge Zhang <tonyseek@gmail.com>
2015-03-09 06:26:01 +0800

    welcome to future: unicode everywhere.

--- rsocks/eventlib.py
+++ rsocks/eventlib.py
@@ -1,5 +1,8 @@
+from __future__ import unicode_literals
+
 from eventlet import import_patched, GreenPool, listen
 from eventlet.green import socket, ssl
+
 
 __all__ = ['socks', 'socket', 'ssl', 'GreenPool', 'listen']
 
--- rsocks/utils.py
+++ rsocks/utils.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import os
 
 from six.moves.urllib.parse import urlparse, parse_qsl
