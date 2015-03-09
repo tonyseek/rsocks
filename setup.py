1b164d49421de88500ccb75977266a3e948e123b
Jiangge Zhang <tonyseek@gmail.com>
2015-03-09 15:48:21 +0800

    Bump version: 0.1.0.dev → 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as readme:
 
 setup(
     name='rsocks',
-    version='0.1.0.dev',
+    version='0.1.0',
     author='Jiangge Zhang',
     author_email='tonyseek@gmail.com',
     description='A SOCKS reserve proxy server.',
