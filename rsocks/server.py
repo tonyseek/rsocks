1b70162215d09a7f81cef5237ffd7210a1d975b7
Jiangge Zhang <tonyseek@gmail.com>
2015-03-09 15:44:52 +0800

    fix up the reading conflict issue.

--- rsocks/server.py
+++ rsocks/server.py
@@ -35,20 +35,20 @@ class Server(object):
         while True:
             try:
                 sock, address = self.server.accept()
-                self.handle_accept(sock, address)
+                self.handle_accept(sock, address, extras=None)
             except (SystemExit, KeyboardInterrupt):
                 self.logger.info('Stopping server...')
                 break
 
-    def handle_accept(self, sock, address):
+    def handle_accept(self, sock, address, extras=None):
         self.logger.info('Connection from %s:%d' % address)
-        self.pool.spawn_n(self.handle_r, sock)
-        self.pool.spawn_n(self.handle_w, sock)
+        self.pool.spawn_n(self.handle_r, sock, extras)
+        self.pool.spawn_n(self.handle_w, sock, extras)
 
-    def handle_r(self, sock):
+    def handle_r(self, sock, extras=None):
         raise NotImplementedError
 
-    def handle_w(self, sock):
+    def handle_w(self, sock, extras=None):
         raise NotImplementedError
 
 
@@ -61,7 +61,6 @@ class ReverseProxyServer(Server):
     def __init__(self, upstream, use_ssl=False, chunk_size=32384):
         super(ReverseProxyServer, self).__init__()
         self.upstream = upstream
-        self.upstream_sock = None
         self.use_ssl = use_ssl
         self.chunk_size = chunk_size
         self.proxy_server = None
@@ -70,44 +69,38 @@ class ReverseProxyServer(Server):
         self.proxy_server = parse_proxy_uri(uri)
         self.logger.info('Using proxy server %s' % printable_uri(uri))
 
-    def handle_accept(self, sock, address):
-        if self.upstream_sock:
-            try:
-                self.upstream_sock.shutdown(socket.SHUT_RDWR)
-            except socket.error as e:
-                if e.args[0] == 57:  # 57 - socket is not connected
-                    self.logger.info('Closed %s:%d' % self.upstream)
-                else:
-                    raise
-            else:
-                self.logger.info('Disconnected from %s:%d' % self.upstream)
-
+    def handle_accept(self, sock, address, extras=None):
         if self.proxy_server:
-            self.upstream_sock = socks.socksocket()
-            self.upstream_sock.set_proxy(**self.proxy_server)
-            self.upstream_sock.connect(self.upstream)
+            upstream_sock = socks.socksocket()
+            upstream_sock.set_proxy(**self.proxy_server)
+            upstream_sock.connect(self.upstream)
             if self.use_ssl:
-                self.upstream_sock = ssl.SSLSocket(self.upstream_sock)
+                upstream_sock = ssl.SSLSocket(upstream_sock)
         else:
-            self.upstream_sock = socket.socket()
+            upstream_sock = socket.socket()
             if self.use_ssl:
-                self.upstream_sock = ssl.SSLSocket(self.upstream_sock)
-            self.upstream_sock.connect(self.upstream)
+                upstream_sock = ssl.SSLSocket(upstream_sock)
+            upstream_sock.connect(self.upstream)
 
-        super(ReverseProxyServer, self).handle_accept(sock, address)
         self.logger.info('Connected to upstream %s:%d' % self.upstream)
+        super(ReverseProxyServer, self).handle_accept(
+            sock, address, extras={'upstream_sock': upstream_sock})
 
-    def handle_r(self, sock):
-        self._forward(sock, self.upstream_sock, 'Sending')
+    def handle_r(self, sock, extras=None):
+        upstream_sock = extras['upstream_sock']
+        self._forward(sock, upstream_sock, 'Sending')
 
-    def handle_w(self, sock):
-        self._forward(self.upstream_sock, sock, 'Received')
+    def handle_w(self, sock, extras=None):
+        upstream_sock = extras['upstream_sock']
+        self._forward(upstream_sock, sock, 'Received')
 
-    def _forward(self, src, dst, label):
+    def _forward(self, src, dst, label, eof_callback=None):
         while True:
             data = src.recv(self.chunk_size)
             if not data:
                 self.logger.debug('%s EOF' % label)
+                if eof_callback:
+                    eof_callback()
                 break
             self.logger.debug('%s %r' % (label, data))
             dst.sendall(data)
